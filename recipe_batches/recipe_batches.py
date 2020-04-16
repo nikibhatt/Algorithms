cf5531e22a486ca0e52b0bfa180a7746a6ec15bb
nikibhatt <nkibhatt@users.noreply.github.com>
2020-04-16 09:30:18 -0700

    completed 2nd problem

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,12 +3,30 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  item_count = 0
+  count = 0
+  for key, value in recipe.items():
+        if key in ingredients:
+            if value > ingredients[key]:
+              return count
+            else:
+              ingredients[key] = ingredients[key] - value
+              item_count += 1
+        else:
+            return count
+  if item_count == len(recipe):
+        count += 1
+
+  add_count = recipe_batches(recipe, ingredients)
+  count = count + add_count
+  return count
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
+  # Change the entries of these dictionaries to test
   # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  #recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+  #ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  recipe = { 'milk': 2, 'sugar': 40, 'butter': 20 }
+  ingredients = { 'milk': 5, 'sugar': 120, 'butter': 500 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,13 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+    best_profit = -9999
+    for i in range(0,len(prices)):
+        for j in range(i+1, len(prices)):
+            profit = prices[j] - prices[i]
+            if profit > best_profit:
+                best_profit = profit
+    return best_profit
 
 
 if __name__ == '__main__':
